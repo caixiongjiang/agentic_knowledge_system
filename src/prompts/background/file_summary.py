47237a84231d6e3b204424bdf5b2a26c70f7b19c
caixiongjiang <1837278011@qq.com>
2026-01-06 13:04:30 +0800

    feat(prompt): 新增多个后台处理prompt

---
+++ src/prompts/background/file_summary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@PROJECT_NAME: agentic_knowledge_system
+@File    : file_summary.py
+@Author  : caixiongjiang
+@Date    : 2026/1/6 11:00
+@Function: 
+    函数功能名称
+@Modify History:
+         
+@Copyright：Copyright(c) 2024-2026. All Rights Reserved
+=================================================="""
---
+++ src/prompts/background/text_analyzer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@PROJECT_NAME: agentic_knowledge_system
+@File    : text_analyzer.py
+@Author  : caixiongjiang
+@Date    : 2026/1/6 11:02
+@Function: 
+    函数功能名称
+@Modify History:
+         
+@Copyright：Copyright(c) 2024-2026. All Rights Reserved
+=================================================="""
